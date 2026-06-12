6d009880ff08e8eb79671d97055dab85640297dd
jodio258 <799303576@qq.com>
2026-06-12 20:32:00 +0800

    build: bootstrap backend skeleton

---
+++ backend/src/main/java/com/jodio/biliagent/BiliAgentApplication.java
@@ -0,0 +1,12 @@
+package com.jodio.biliagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiliAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiliAgentApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/jodio/biliagent/BiliAgentApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jodio.biliagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiliAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
