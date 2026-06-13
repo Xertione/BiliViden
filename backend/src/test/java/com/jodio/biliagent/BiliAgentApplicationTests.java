a2ee428ebf840b34148cd8f66eaa49319e183820
jodio258 <799303576@qq.com>
2026-06-13 09:56:34 +0800

    fix: stabilize backend skeleton tests

--- backend/src/test/java/com/jodio/biliagent/BiliAgentApplicationTests.java
+++ backend/src/test/java/com/jodio/biliagent/BiliAgentApplicationTests.java
@@ -3,7 +3,7 @@ package com.jodio.biliagent;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.redisson.spring.starter.RedissonAutoConfigurationV2")
 class BiliAgentApplicationTests {
 
     @Test
