415c8e08a2a61c92b04099a6cf597ea17b682020
jodio258 <799303576@qq.com>
2026-06-13 10:23:00 +0800

    test: activate test profile for backend context test

--- backend/src/test/java/com/jodio/biliagent/BiliAgentApplicationTests.java
+++ backend/src/test/java/com/jodio/biliagent/BiliAgentApplicationTests.java
@@ -2,8 +2,10 @@ package com.jodio.biliagent;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = "spring.autoconfigure.exclude=org.redisson.spring.starter.RedissonAutoConfigurationV2")
+@ActiveProfiles("test")
 class BiliAgentApplicationTests {
 
     @Test
